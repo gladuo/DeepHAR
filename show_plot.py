fe839e6f2d1f74eba2f10aab08fe2eebbf062f7b
Gladuo <me@gladuo.com>
2017-04-21 20:38:28 +0800

    add network plot

--- show_plot.py
+++ show_plot.py
@@ -1,2 +1,19 @@
 # coding=utf-8
 
+from keras import models
+from keras.utils.vis_utils import plot_model
+
+from config import *
+
+
+def show_plot():
+    # Load model
+    json_string = open('./runs/{}/model_pickle.json'.format(str(TEST_MODEL_NUMBER)), 'r').read()
+    model = models.model_from_json(json_string)
+
+    # Show model
+    plot_model(model, to_file='%s.png' % TEST_MODEL_NUMBER)
+
+
+if __name__ == '__main__':
+    show_plot()
